b2dfde1cdd00d0ced4bb4eeb8de9643fa34880a0
Mohima-Mitra <147180904+Mohima-Mitra@users.noreply.github.com>
2024-09-12 21:58:52 +0530

    Create ImplementQueueUsingStack.java

---
+++ ImplementQueueUsingStack.java
@@ -0,0 +1,43 @@
+class MyQueue {
+    private Stack<Integer> main;
+    private Stack<Integer> helper;
+
+    public MyQueue() {
+        main= new Stack<>();
+        helper=new Stack<>();
+    }
+    
+    public void push(int x) {
+        //moving all  elements from main stack to helper
+        while(main.size()>0){
+          helper.push(main.pop());
+        }
+        //adding element to main
+        main.push(x);
+        //moving all elements from helper to main stack
+        while(helper.size()>0){
+            main.push(helper.pop());
+        }
+
+
+    }
+    
+    public int pop() {
+        return main.pop();
+        
+    }
+    
+    public int peek() {
+        return main.peek();
+        
+    }
+    
+    public boolean empty() {
+        if(main.size()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
